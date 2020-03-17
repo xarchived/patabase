1af2a977d3de5e79972d03a3771dd5b433a3c634
Xurvan <xurvan@tuta.io>
2020-03-16 20:52:12 -0700

    As we use "RealDictCursor" we don't have to convert rows to dict

--- patabase/postgres.py
+++ patabase/postgres.py
@@ -39,7 +39,7 @@ class Database(object):
             rows = cur.fetchall()
 
         for row in rows:
-            yield dict(row)
+            yield row
 
     def procedure(self, func_name: str, **parameters: any) -> int:
         with self._con.cursor() as cur:
@@ -54,4 +54,4 @@ class Database(object):
             rows = cur.fetchall()
 
         for row in rows:
-            yield dict(row)
+            yield row
